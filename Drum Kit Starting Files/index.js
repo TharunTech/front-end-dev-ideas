d50bc0c9850b6991950b16d5d0d5bab4d3892a62
Tharun <tharuntech9840@gmail.com>
2022-05-05 18:36:41 +0530

    s5

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,19 @@
+
+
+for(var a=0 ; a<document.querySelectorAll(".drum").length ; a++){
+
+    document.querySelectorAll("button")[a].addEventListener("click",function() {
+    
+        this.style.color="white";
+
+    });
+
+}
+
+
+
+
+
+// var audio = new Audio("./sounds/tom-1.mp3")
+// audio.play();
+// new
