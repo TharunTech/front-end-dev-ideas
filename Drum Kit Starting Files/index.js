4d4ab7acfe560c6ddb019b3220b4327b01804196
Tharun <tharuntech9840@gmail.com>
2022-05-22 21:04:32 +0530

    s1

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -3,6 +3,41 @@
 for(var a=0 ; a<document.querySelectorAll(".drum").length ; a++){
 
     document.querySelectorAll("button")[a].addEventListener("click",function() {
+        var buttonInnerHTML = this.innerHTML;
+
+        switch (buttonInnerHTML) {
+            case "w":
+                var tom1 = new Audio("./sounds/tom-1.mp3")
+                tom1.play();
+                break;
+            case "a":
+                var tom2 = new Audio("./sounds/tom-2.mp3")
+                top2.play();
+                break;
+            case "s":
+                var tom3 = new Audio("./sounds/tom-3.mp3")
+                tom3.play();
+                break;
+            case "d":
+                var tom4 = new Audio("./sounds/tom-4.mp3")
+                tom4.play();
+                break;
+            case "j":
+                var snare = new Audio("./sounds/snare.mp3")
+                snare.play();
+                break;
+            case "k":
+                var crash = new Audio("./sounds/crash.mp3")
+                crash.play();
+                break;
+            case "l":
+                var kick = new Audio("./sounds/kick-bass.mp3")
+                kick.play();
+                break;
+            default:
+                console.log(buttonInnerHTML);
+                break;
+        }
     
         this.style.color="white";
 
