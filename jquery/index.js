748a5c1f0bb41562d7f8b4a76a47ca333daebefa
Tharun <tharuntech9840@gmail.com>
2022-07-26 22:18:07 +0530

    s3

---
+++ jquery/index.js
@@ -0,0 +1,7 @@
+
+
+// $(document).ready(function(){//if jquery script not at the end , tat is if its in the begning  or headder just include this function to load the library first.
+//     $("h1").css("color","red");
+// })
+
+$("h1").html("<em>heloooo</em>")
